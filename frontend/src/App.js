e57ba5daac12c5cc2e27decb9a87c90dffda2291
MasulukeI <imasuluke@retrorabbit.co.za>
2019-01-30 12:38:57 +0200

    web worker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.scss';
 import axios from 'axios'
 import { Route, Link } from 'react-router-dom';
+import myWorker from './test.worker';
 
 class App extends Component {
 
@@ -15,7 +16,11 @@ class App extends Component {
 
     this.handleClick = this.handleClick.bind(this)
   }
-
+  componentDidMount() {
+    const worker = new myWorker();
+    worker.postMessage(this.state.counter);
+    worker.addEventListener('message', event => this.setState({counter: event.data}));
+}
   handleClick () {
     axios.get('https://api.github.com/users/MasulukeI')
       .then(response => this.setState({username: response.data.name}))
