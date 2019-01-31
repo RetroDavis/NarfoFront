3ea373dabfd246cc6ec95e85080d107e082f05c3
MasulukeI <imasuluke@retrorabbit.co.za>
2019-01-31 09:25:34 +0200

    front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render( <Router>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto ">
           <li class="nav-item">
-            <Link class="nav-link" to="/Home">Home</Link>
+            <Link class="nav-link" to="/">Home</Link>
           </li>
           <li class="nav-item">
           <Link class="nav-link" to="/About">About</Link>
