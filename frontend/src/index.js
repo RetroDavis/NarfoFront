676c34bdfe6e550d3dfa8ad207cb8087943597f2
LindanyRetroRabbit <lmabaso@retrorabbit.co.za>
2019-01-31 14:39:43 +0200

    Updating webservices

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -54,7 +54,7 @@ ReactDOM.render( <Router>
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="js/clean-blog.min.js"></script>
-    <script src="js/global.js"></script>
+    <script src="js/main.js"></script>
     </div>
 </Router>,
   document.getElementById('root'));
