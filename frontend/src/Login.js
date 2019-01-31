4b2c3a5108aab9c64dc140298530470112c7ccbd
DAVIS MUDAU <nmudau@Retrorabbit.co.za>
2019-01-31 15:46:00 +0200

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,12 +5,45 @@ import axios from 'axios'
 
 class Login extends Component {
   state = {
-    members: []
+    members: {
+      firstName: '',
+      lastName: '',
+      userName: '',
+      password: ''
+  }
   }
   constructor () {
     var d = 5;
    super()
   }
+
+
+  handleChangeFirstName = event => {
+  this.state.members.firstName =  event.target.value;
+}
+handleChangeLastName = event => {
+    this.state.members.lastName = event.target.value;
+}
+handleChangeUserName = event => {
+    this.state.members.userName = event.target.value;
+}
+handleChangePassWord = event => {
+    this.state.members.password = event.target.value;
+}
+
+
+handleSubmit = event => {
+    event.preventDefault();
+  
+
+    
+    const user = this.state.members;
+    console.log(user);
+    axios.post('https://localhost:44341/Member/set', { user })
+        .then(res => {
+            console.log(res.data);
+        });
+};
 
   handleClick () {
     axios.get('https://api.github.com/users/MasulukeI')
@@ -40,15 +73,14 @@ class Login extends Component {
         <div class="container">
           <div class="row">
           <div class="wrapper">
-          <form class="form-signin">       
-            <h2 class="form-signin-heading text-success">Register Member</h2>
-            <input type="text" class="form-control text-center" name="firstName" placeholder="Enter FirstName" required="" autofocus="" />
-            <input type="text" class="form-control text-center" name="lastName" placeholder="Enter LastName" required="" autofocus="" />
-             <input type="text" class="form-control text-center" name="userName" placeholder="Enter Username" required="" autofocus="" />
-            <input type="password" class="form-control text-center" name="password" placeholder="Enter Password" required=""/> 
+             <form class="form-signin" onSubmit={this.handleSubmit}>       
+        <h2 class="form-signin-heading text-success">Register Member</h2>
+                        <input type="text"   class="form-control text-center" onChange={this.handleChangeFirstName} name="firstName" placeholder="Enter FirstName" required="" autofocus="" />
+                        <input type="text"  class="form-control text-center" onChange={this.handleChangeLastName} name="lastName" placeholder="Enter LastName" required="" autofocus="" />
+                        <input type="text" class="form-control text-center" onChange={this.handleChangeUserName} name="userName" placeholder="Enter Username" required="" autofocus="" />
+                        <input type="password" class="form-control text-center" onChange={this.handleChangePassWord} name="password" placeholder="Enter Password" required=""/> 
 
-            <button class="btn btn-lg btn-success btn-block" type="submit">Register</button>   
-          </form>
+        <button class="btn btn-lg btn-success btn-block" type="submit">Register</button> </form>
         </div>
           </div>
         </div>
