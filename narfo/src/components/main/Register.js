b5be27009bc6bcb372c7fd442485a7be61b3a2d9
CastelloGovender <cgovender@retrorabbit.co.za>
2019-02-07 12:11:27 +0200

    Created Template Registraton Page

--- narfo/src/components/main/Register.js
+++ narfo/src/components/main/Register.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import MainRegister  from '../registerPage/MainRegister';
 
-export default class Register extends Component {
+const styles = theme => ({
+  root: {
+    // width: '100%',
+    // height: '100%',
+    // flex:1,
+    height: '100vh',
+  },
+});
+
+class Register extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Register Page</h1>
+      <div className={classes.root}>
+        <MainRegister></MainRegister>
       </div>
     )
   }
 }
+
+Register.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Register);
