dc0c258bce48ac3614b016876588b1d0370c2332
MasulukeI <imasuluke@retrorabbit.co.za>
2019-02-12 08:24:51 +0200

    mapped array to select

--- narfo/src/components/memberapplication/SalesDetails.js
+++ narfo/src/components/memberapplication/SalesDetails.js
@@ -5,11 +5,11 @@ import InputLabel from "@material-ui/core/InputLabel";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import Checkbox from "@material-ui/core/Checkbox";
 import Radio from "@material-ui/core/Radio";
-import Fab from '@material-ui/core/Fab';
-import NavigateNext from '@material-ui/icons/NavigateNext';
-import ArrowBack from '@material-ui/icons/NavigateBefore';
+import Fab from "@material-ui/core/Fab";
+import NavigateNext from "@material-ui/icons/NavigateNext";
+import ArrowBack from "@material-ui/icons/NavigateBefore";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class SalesDetails extends Component {
   constructor(props) {
@@ -19,24 +19,13 @@ class SalesDetails extends Component {
       Sales_Representitive: "",
       Sales_list: "",
       Store: "",
-      Store_list: "",
+      Store_list: [],
       CurrentMember_Associaton: false,
       communication_Policy: false,
       labelWidth: 0,
       isLoaded: false,
-      CallResult: [],
+      CallResult: []
     };
-  }
-
-  componentDidMount(){
-    fetch('http://localhost:44327/api/salesreps/get/all/sales')
-    .then(res => res.json())
-    .then(json => {
-        this.setState({
-            isLoaded: true,
-            CallResult: json
-        })
-    });
   }
 
   handleOptionChange = event => {
@@ -44,12 +33,33 @@ class SalesDetails extends Component {
   };
 
   handleChange = name => event => {
-    this.props.onMemberDetailsChange(event.target.value,name);;
+    this.props.onMemberDetailsChange(event.target.value, name);
   };
 
   handlePolicyOption = name => event => {
-    this.props.onMemberDetailsChange(event.target.checked,name);;
+    this.props.onMemberDetailsChange(event.target.checked, name);
   };
+
+  componentDidMount(){
+    fetch('https://localhost:44327/api/salesReps/get/all/sales')
+    .then(res => res.json())
+    .then(json => {
+    this.setState({
+    isLoaded: true,
+    CallResult: json.members
+    })
+    });
+    
+    fetch('https://localhost:44327/api/Branches')
+    .then(res => res.json())
+    .then(json => {
+    this.setState({
+    isLoaded: true,
+    Store_list: json.branchName
+    })
+    });
+    
+    }
 
   handleInputChange = event => {
     event.preventDefault();
@@ -57,10 +67,10 @@ class SalesDetails extends Component {
       [event.target.name]: event.target.value
     });
   };
-  handleSubmit = (event) => {
-   event.preventDefault()
-   console.log(this.state.CallResult);
-}
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.CallResult);
+  };
 
   render() {
     let { isLoaded, items } = this.state;
@@ -69,15 +79,16 @@ class SalesDetails extends Component {
     const Sales_list = ["hello", "goodbye"];
     const { Store } = this.state;
     const { CurrentMember_Associaton } = this.state;
-  
+
     // let Sales_list = this.props.state.Sales_list;
     // let optionItems = Sales_list.map((planet) =>
     //         <option key={planet.name}>{planet.name}</option>
     // );
     return (
       <div>
-         <h1>Membership Application</h1>
-        <h2>Sales decleration</h2><br/>
+        <h1>Membership Application</h1>
+        <h2>Sales decleration</h2>
+        <br />
         <form onSubmit={this.handleSubmit}>
           <FormControl variant="outlined">
             <InputLabel
@@ -100,11 +111,9 @@ class SalesDetails extends Component {
                 />
               }
             >
-                             <option value={"Gauteng"}>Gauteng</option>
-              <option value={"Limpopo"}>Limpopo</option>
-              <option value={"CapeTown"}>CapeTown</option>
-              <option value={"durbs"}>durbs</option>
-              <option value={"EP"}>EP</option>
+              {this.state.CallResult.map((name, index) => (
+                <option value={name.firstname}>{name.firstname}</option>
+              ))}
             </Select>
           </FormControl>
           <br />
@@ -130,12 +139,9 @@ class SalesDetails extends Component {
                 />
               }
             >
-              <option value="Store" />
-              <option value={"Gauteng"}>Gauteng</option>
-              <option value={"Limpopo"}>Limpopo</option>
-              <option value={"CapeTown"}>CapeTown</option>
-              <option value={"durbs"}>durbs</option>
-              <option value={"EP"}>EP</option>
+              {this.state.Store_list.map((name, index) => (
+                <option value={name.branch1}>{name.branch1}</option>
+              ))}
             </Select>
           </FormControl>{" "}
           <br />
@@ -144,7 +150,7 @@ class SalesDetails extends Component {
             <br />
             <Radio
               checked={this.props.memDetails.CurrentMember_Associaton === "Yes"}
-              onChange={this.handleChange('CurrentMember_Associaton')}
+              onChange={this.handleChange("CurrentMember_Associaton")}
               value="Yes"
               name="radio-button-demo"
               aria-label="Yes"
@@ -152,7 +158,7 @@ class SalesDetails extends Component {
             Yes
             <Radio
               checked={this.props.memDetails.CurrentMember_Associaton === "No"}
-              onChange={this.handleChange('CurrentMember_Associaton')}
+              onChange={this.handleChange("CurrentMember_Associaton")}
               value="No"
               name="radio-button-demo"
               aria-label="No"
@@ -168,7 +174,7 @@ class SalesDetails extends Component {
               value="communication_Policy"
             />
           </label>
-          <br/>
+          <br />
           <Link to="/Declaration">
             <Fab color="primary" aria-label="Add" type="Submit">
               <ArrowBack />
@@ -186,16 +192,25 @@ class SalesDetails extends Component {
 }
 const mapStateToProps = state => {
   return {
-      currPage: state.currentPage,
-      memDetails: state.signupDetails
+    currPage: state.currentPage,
+    memDetails: state.signupDetails
   };
-}
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-      onPageChange: (pageName) => dispatch({type: 'UPDATE_CURRENT_PAGE', currPage:pageName}),
-      onMemberDetailsChange: (value,vname) => dispatch({type: 'UPDATE_MEMBER_DETAILS', varValue:value, varName:vname})
-  }
-}
+    onPageChange: pageName =>
+      dispatch({ type: "UPDATE_CURRENT_PAGE", currPage: pageName }),
+    onMemberDetailsChange: (value, vname) =>
+      dispatch({
+        type: "UPDATE_MEMBER_DETAILS",
+        varValue: value,
+        varName: vname
+      })
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SalesDetails);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SalesDetails);
