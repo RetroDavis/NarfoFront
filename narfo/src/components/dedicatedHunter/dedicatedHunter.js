baed32e361cf85608416621cbc3cd79befc118d2
DAVIS MUDAU <nmudau@Retrorabbit.co.za>
2019-02-12 17:45:09 +0200

    updated

--- narfo/src/components/dedicatedHunter/dedicatedHunter.js
+++ narfo/src/components/dedicatedHunter/dedicatedHunter.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 class DedicatedHunter extends Component {
+   state = {};
+    constructor()
+    {
+        super();
+        this.state = {
+            CurrentPage : 0
+        }
+        this.NextPage = this.NextPage.bind(this);
+    }
+  
+      
+  NextPage()
+  {
+      const newPage =1 + this.state.CurrentPage;
+      this.setState({CurrentPage  : newPage})
+     console.log(newPage);
+  }
   render() {
     return (
-
- <div className="container mt-3 shadow">
+<div>
+{(this.state.CurrentPage === 1 ) && <div className="container mt-3 shadow">
 
     <div className="row">
         <div className="col mt-3">
@@ -33,13 +52,60 @@ class DedicatedHunter extends Component {
 
         <div className="row">
                 <div className="col mb-5">
-                <button className="btn btn-dark  btn-block">
+                <button className="btn btn-dark  btn-block" onClick={this.NextPage}>
                     APPLY
                 </button>
                 </div>
         </div>
    
-  </div>);
+  </div>}
+  {(this.state.CurrentPage === 0 ) && <div className="container mt-3 shadow">
+
+  <div className="row mb-3">
+      <div className="col mt-3">
+          <h3 className="text-center">Dedicated Status Application</h3>
+       </div>
+       </div>
+     <div className="row mb-4">
+
+      <div className="col">
+      <div className="row mb-2">
+      <div className="col shadow">
+     <p className="text-left ml-3">Do you currently hold a dedicated hunter status with any other accreddited association </p>
+      <div className="row form-check-inline text-left">
+    <label class="form-check-label"><input type="radio" class="form-check-input custom-radio bg-secondary" name="optradio" /> Yes</label>
+    <label class="form-check-label"><input type="radio" class="form-check-input custom-radio bg-secondary" name="optradio" /> No</label>
+       
+ 
+      </div>
+     </div>
+       </div>
+       </div>
+
+
+
+      </div>       
+      
+
+      <div className="row mb-2">
+        <div className="col  text-danger text-center"> </div>
+      </div>
+
+
+      <div className="row">
+              <div className="col mb-5">
+              <button className="btn btn-dark  btn-block" onClick={this.NextPage}>
+                 Next
+              </button>
+              </div>
+      </div>
+ 
+</div>}
+
+
+  </div>
+  
+);
   }
 }
 
